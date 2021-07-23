e43379e65642dc4efd30de22f1df119ec340b035
hiteshreekoshiya <hiteshreekoshiya123@gmail.com>
2021-07-23 10:43:12 +0530

    config file added

---
+++ src/config.jsx
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyChOtv9BX8fG6HSl8NqPi4YBwx0h86MMTk",
+    authDomain: "book-list-6030a.firebaseapp.com",
+    projectId: "book-list-6030a",
+    storageBucket: "book-list-6030a.appspot.com",
+    messagingSenderId: "337332669968",
+    appId: "1:337332669968:web:701ff00fcbcba4ef194495"
+};
+
+export default firebaseConfig;
